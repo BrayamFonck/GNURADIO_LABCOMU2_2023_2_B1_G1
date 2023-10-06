ed7b8bfe3a84f231d89d5fb22c73cc66eda9ab81
BrayamFonck <115673041+BrayamFonck@users.noreply.github.com>
2023-10-06 09:52:16 -0500

    FIN

--- Proyecto5/flujograma.py
+++ Proyecto5/flujograma.py
@@ -37,6 +37,7 @@ from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio.qtgui import Range, RangeWidget
 from PyQt5 import QtCore
+import E3TRadio
 import math
 import time
 import threading
@@ -106,6 +107,54 @@ class flujograma(gr.top_block, Qt.QWidget):
         self._noise_range = Range(0, 1, 0.0125, 0, 200)
         self._noise_win = RangeWidget(self._noise_range, self.set_noise, "Ruido", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._noise_win)
+        self.qtgui_time_sink_x_1 = qtgui.time_sink_f(
+            1024, #size
+            samp_rate, #samp_rate
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_1.set_update_time(0.10)
+        self.qtgui_time_sink_x_1.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_1.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_1.enable_tags(True)
+        self.qtgui_time_sink_x_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_1.enable_autoscale(False)
+        self.qtgui_time_sink_x_1.enable_grid(False)
+        self.qtgui_time_sink_x_1.enable_axis_labels(True)
+        self.qtgui_time_sink_x_1.enable_control_panel(False)
+        self.qtgui_time_sink_x_1.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_1.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_1.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_1.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_1.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_1.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_1.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_1.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_1_win)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
             1024, #size
             samp_rate, #samp_rate
@@ -278,10 +327,24 @@ class flujograma(gr.top_block, Qt.QWidget):
         self.blocks_vector_sink_x_0 = blocks.vector_sink_b(1, 1024)
         self.blocks_unpacked_to_packed_xx_0 = blocks.unpacked_to_packed_bb(2, gr.GR_MSB_FIRST)
         self.blocks_packed_to_unpacked_xx_0 = blocks.packed_to_unpacked_bb(bps, gr.GR_MSB_FIRST)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(1/Sps)
+        self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
         self.blocks_add_xx_0_0 = blocks.add_vcc(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.analog_noise_source_x_0_0 = analog.noise_source_c(analog.GR_GAUSSIAN, noise, 0)
         self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, noise, 0)
+        self.Menu = Qt.QTabWidget()
+        self.Menu_widget_0 = Qt.QWidget()
+        self.Menu_layout_0 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.Menu_widget_0)
+        self.Menu_grid_layout_0 = Qt.QGridLayout()
+        self.Menu_layout_0.addLayout(self.Menu_grid_layout_0)
+        self.Menu.addTab(self.Menu_widget_0, 'Time & Freq')
+        self.Menu_widget_1 = Qt.QWidget()
+        self.Menu_layout_1 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.Menu_widget_1)
+        self.Menu_grid_layout_1 = Qt.QGridLayout()
+        self.Menu_layout_1.addLayout(self.Menu_grid_layout_1)
+        self.Menu.addTab(self.Menu_widget_1, 'Freq')
+        self.top_layout.addWidget(self.Menu)
         self._Label_0_tool_bar = Qt.QToolBar(self)
 
         if None:
@@ -304,25 +367,30 @@ class flujograma(gr.top_block, Qt.QWidget):
         self._Label_label = Qt.QLabel(str(self._Label_formatter(self.Label)))
         self._Label_tool_bar.addWidget(self._Label_label)
         self.top_layout.addWidget(self._Label_tool_bar)
+        self.E3TRadio_diezmador_cc_0 = E3TRadio.diezmador_cc(8)
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.E3TRadio_diezmador_cc_0, 0), (self.digital_constellation_decoder_cb_0, 0))
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.analog_noise_source_x_0_0, 0), (self.blocks_add_xx_0_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.qtgui_const_sink_x_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_add_xx_0_0, 0), (self.digital_constellation_decoder_cb_0, 0))
+        self.connect((self.blocks_add_xx_0_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_1, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.E3TRadio_diezmador_cc_0, 0))
         self.connect((self.blocks_packed_to_unpacked_xx_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
+        self.connect((self.blocks_unpacked_to_packed_xx_0, 0), (self.blocks_char_to_float_0, 0))
         self.connect((self.blocks_unpacked_to_packed_xx_0, 0), (self.blocks_vector_sink_x_0, 0))
         self.connect((self.blocks_unpacked_to_packed_xx_0, 0), (self.prob1, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_packed_to_unpacked_xx_0, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.prob, 0))
-        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.blocks_add_xx_0_0, 1))
         self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.interp_fir_filter_xxx_0, 0))
         self.connect((self.digital_constellation_decoder_cb_0, 0), (self.blocks_unpacked_to_packed_xx_0, 0))
         self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_add_xx_0_0, 1))
         self.connect((self.interp_fir_filter_xxx_0, 0), (self.qtgui_freq_sink_x_0, 0))
 
 
@@ -377,6 +445,7 @@ class flujograma(gr.top_block, Qt.QWidget):
         self.Sps = Sps
         self.set_h([1]*self.Sps)
         self.set_samp_rate(self.RS*self.Sps)
+        self.blocks_multiply_const_vxx_0.set_k(1/self.Sps)
 
     def get_RS(self):
         return self.RS
@@ -399,6 +468,7 @@ class flujograma(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate)
 
     def get_noise(self):
         return self.noise
